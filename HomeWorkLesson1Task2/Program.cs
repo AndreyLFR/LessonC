8ae3e91d8dbbc4a01dd7b500e90c0c4ad43289da
LFR <ooolfr@mail.ru>
2022-07-15 17:21:56 +0300

    Home Work Task 2 complete

---
+++ HomeWorkLesson1Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("a=");
+string? inputLkneOne=Console.ReadLine();
+Console.Write("b=");
+string? inputLkneTwo=Console.ReadLine();
+
+if (inputLkneOne!=null && inputLkneTwo!=null)
+{
+    int a=int.Parse(inputLkneOne);
+    int b=int.Parse(inputLkneTwo);
+
+    if(a<b)
+    {
+        Console.Write("max=");
+        Console.Write(b);
+    }
+    else
+    {
+        Console.Write("max=");
+        Console.Write(a);
+    }
+}
