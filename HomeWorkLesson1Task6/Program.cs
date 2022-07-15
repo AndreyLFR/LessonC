e73b04877c20b2af66e93b6ff7c05bca39010024
LFR <ooolfr@mail.ru>
2022-07-15 17:47:53 +0300

    HomeWork Task6 complete

---
+++ HomeWorkLesson1Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+string? inputLkneNumber=Console.ReadLine();
+
+if(inputLkneNumber!=null)
+{
+    int number=int.Parse(inputLkneNumber);
+    int resultDivision=number%2;
+
+        if(resultDivision==0)
+        {
+            Console.WriteLine("Введенное число - четное");            
+        }
+        else{
+            Console.WriteLine("Введенное число - нечетное"); 
+        }
+}
