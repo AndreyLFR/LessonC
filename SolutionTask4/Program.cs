7a39d830621cf3d33f7d2d576b3d3d9550391fc6
LFR <ooolfr@mail.ru>
2022-07-15 14:40:37 +0300

    add task4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите  номер:");
+string? inputLkneNumber=Console.ReadLine();
+
+if(inputLkneNumber!=null)
+{
+    int number=int.Parse(inputLkneNumber);
+    int otatok=number%10;
+
+    Console.WriteLine(otatok);
+}
