5fef78ff153747a9e54508859c2d2a0296785f5e
LFR <ooolfr@mail.ru>
2022-07-15 12:37:38 +0300

    add solution for task 0

---
+++ NewFolder/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите число:");
+string? inputLine=Console.ReadLine();
+
+if(inputLine!=null)
+{
+int inputNumber=int.Parse(inputLine);
+//int outNumber=inputNumber*inputNumber;
+int outNumber=(int)Math.Pow(inputNumber,2);
+Console.WriteLine(outNumber);
+}
