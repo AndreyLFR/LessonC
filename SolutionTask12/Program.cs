a8bceb83dd027449330bb4247ea15f4092a9419d
LFR <ooolfr@mail.ru>
2022-07-15 23:42:26 +0300

    task 12 остаток от деления

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите  число 1:");
+string? inputOneNumber=Console.ReadLine();
+Console.WriteLine("Введите  число 2:");
+string? inputTwoNumber=Console.ReadLine();
+
+if(inputOneNumber!=null && inputTwoNumber!=null)
+{
+    int oneNumber=int.Parse(inputOneNumber);
+    int twoNumber=int.Parse(inputTwoNumber);
+    int c=twoNumber%oneNumber;
+    Console.WriteLine(c);
+    if(c==0)
+    {
+        Console.WriteLine("yes");
+    }
+    else{
+        Console.WriteLine("no,  остаток"+c);
+    }
+}
