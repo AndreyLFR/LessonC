dee06a0807fd21312b05d8904545828e113dd112
LFR <ooolfr@mail.ru>
2022-07-15 23:18:24 +0300

    Task11, исключение цифры из числа

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,10 @@
+﻿
+System.Random numberSintezator=new Random();
+char[] digits=numberSintezator.Next(100,999).ToString().ToCharArray();
+
+int firstNumber=((int)digits[0])-48;
+int threeNumber=((int)digits[2])-48;
+
+Console.WriteLine(digits);
+Console.Write(firstNumber);
+Console.Write(threeNumber);
