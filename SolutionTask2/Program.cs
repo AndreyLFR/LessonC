fccfb469dec44beb6b10b2a2c92314ac5681382c
LFR <ooolfr@mail.ru>
2022-07-15 13:42:38 +0300

    Add solution task2

---
+++ SolutionTask2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите  номер:");
+string? inputLkneNumber=Console.ReadLine();
+string [] dayOfWeek=new string[7];
+dayOfWeek[0]="Пн";
+dayOfWeek[1]="Вт";
+dayOfWeek[2]="Ср";
+dayOfWeek[3]="Чт";
+dayOfWeek[4]="Пт";
+dayOfWeek[5]="Сб";
+dayOfWeek[6]="Вс";
+
+if (inputLkneNumber!=null)
+{
+    int inputNumber=int.Parse(inputLkneNumber);
+
+    Console.WriteLine(dayOfWeek[inputNumber-1]);
+
+}
