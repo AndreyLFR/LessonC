0488f399ed9a9a6ad23abc28a5398936cb0eed42
LFR <ooolfr@mail.ru>
2022-07-15 17:39:17 +0300

    HomeWork Task4 complete

---
+++ HomeWorkLesson1Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите 1 целое число");
+string? inputLkneOne=Console.ReadLine();
+Console.Write("Введите 2 целое число");
+string? inputLkneTwo=Console.ReadLine();
+Console.Write("Введите 3 целое число");
+string? inputLkneThree=Console.ReadLine();
+
+if (inputLkneOne!=null && inputLkneTwo!=null && inputLkneThree!=null)
+{
+    int oneNumber=int.Parse(inputLkneOne);
+    int twoNumber=int.Parse(inputLkneTwo);
+    int threeNumber=int.Parse(inputLkneThree);
+
+    if(oneNumber<twoNumber)
+    {
+        if(twoNumber<threeNumber)
+        {
+            Console.Write("max=");
+            Console.Write(threeNumber); 
+        }
+        else
+        {            
+            Console.Write("max=");
+            Console.Write(twoNumber);
+        }
+    }
+    else
+    {
+        if(oneNumber<threeNumber)
+        {
+            Console.Write("max=");
+            Console.Write(threeNumber); 
+        }
+        else
+        {            
+            Console.Write("max=");
+            Console.Write(oneNumber);
+        }
+    }
+}
