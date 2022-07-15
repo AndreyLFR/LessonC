b745a5f94f134b1afc9cd7a78a0d15e65a3c2549
LFR <ooolfr@mail.ru>
2022-07-15 13:16:16 +0300

    Add SolutionTask1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1 число:");
+string? inputLkneOne=Console.ReadLine();
+Console.WriteLine("Введите 2 число:");
+string? inputLkneTwo=Console.ReadLine();
+
+if (inputLkneOne!=null && inputLkneTwo!=null)
+{
+    int inputOne=int.Parse(inputLkneOne);
+    int inputTwo=int.Parse(inputLkneTwo);
+
+    if(inputTwo==Math.Sqrt(inputOne))
+    {
+        Console.WriteLine("Верно");
+    }
+    else
+    {
+        Console.WriteLine("Неверно");
+    }
+}
