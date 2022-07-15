1b174de93f30d06172cb8a39d4446566813eefff
LFR <ooolfr@mail.ru>
2022-07-15 18:08:36 +0300

    HomeWor task8 complete

---
+++ HomeWorkLesson1Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число N=");
+string? inputLkneNumber=Console.ReadLine();
+int start=2;
+
+if(inputLkneNumber!=null)
+{
+    int n=int.Parse(inputLkneNumber);
+    int finish=n+1;
+    
+    while(start<finish)
+    {
+        Console.WriteLine(start); 
+        start=start+2;
+    }
+}
