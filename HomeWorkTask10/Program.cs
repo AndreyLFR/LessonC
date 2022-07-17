35a579192db5cf28e20f72f3b48cd8d9370675fa
LFR <ooolfr@mail.ru>
2022-07-17 15:13:52 +0300

    task10  try catch, поиск 3 цифры в числе

---
+++ HomeWorkTask10/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+string? input=Console.ReadLine();
+
+//вариант через индекс массива
+int Variant1(string number)
+{
+char[] arr = number.ToCharArray();
+int numberThree=arr[2]-48;
+Console.WriteLine(numberThree);
+return numberThree;
+}
+//Вариант через остаток от деления
+int Variant2(string number)
+{
+int numberInt=int.Parse(number);
+int div=numberInt%10;
+return div;
+}
+
+try
+{
+//int findNumber=Variant1(input);
+int findNumber=Variant2(input);
+Console.WriteLine(findNumber);
+}
+catch
+//если input==null, то идем сюда
+{
+    Console.WriteLine("Возникло исключение!");
+}
