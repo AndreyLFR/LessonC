5289b7fc2ed0538d149918f4ff1a3fe94b155888
LFR <ooolfr@mail.ru>
2022-07-17 20:38:52 +0300

    task15 complete

---
+++ HomeWorkTask15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+string? input=Console.ReadLine();
+
+try
+{
+    int numberDay=int.Parse(input);
+    if(numberDay==6||numberDay==7)
+    {
+        Console.WriteLine("Да");
+    }
+    else{
+        Console.WriteLine("Нет");
+    }
+}
+catch
+//если input==null, то идем сюда
+{
+    Console.WriteLine("Возникло исключение!");
+}
