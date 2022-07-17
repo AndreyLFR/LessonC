04554993c7a87eaec286260887c07c3eb65c6f6c
LFR <ooolfr@mail.ru>
2022-07-17 12:39:31 +0300

    task16 проверка 1 квадрат 2

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите  число 1:");
+string? inputOneNumber=Console.ReadLine();
+Console.WriteLine("Введите  число 2:");
+string? inputTwoNumber=Console.ReadLine();
+
+if(inputOneNumber!=null && inputTwoNumber!=null)
+{
+    int oneNumber=int.Parse(inputOneNumber);
+    int twoNumber=int.Parse(inputTwoNumber);
+    int oneNumberAbs=oneNumber*oneNumber;
+    int twoNumberAbs=twoNumber*twoNumber;
+
+    if(twoNumberAbs==oneNumber)
+    {
+        Console.WriteLine("1 квадрат 2");
+    }
+    else{
+        Console.WriteLine("1 не квадрат 2");
+    }
+    if(oneNumberAbs==twoNumber)
+    {
+        Console.WriteLine("2 квадрат 1");
+    }
+    else{
+        Console.WriteLine("2 не квадрат 1");
+    }
+}
