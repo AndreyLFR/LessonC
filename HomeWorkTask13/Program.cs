b4876643386772ba4e5744495ce40daac4420981
LFR <ooolfr@mail.ru>
2022-07-17 20:24:32 +0300

    task13  третья цифра в числе

---
+++ HomeWorkTask13/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+string? input=Console.ReadLine();
+
+//вариант через индекс массива
+int Variant1(string number)
+{
+char[] arr = number.ToCharArray();
+if (arr.Length>2)
+    {int numberThree=arr[2]-48;
+    return numberThree;}
+else{
+    int e=-1;
+    return e;
+}
+}
+
+try
+{
+int findNumber=Variant1(input);
+
+if (findNumber==-1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+    {Console.WriteLine(findNumber);
+    }
+}
+catch
+//если input==null, то идем сюда
+{
+    Console.WriteLine("Возникло исключение!");
+}
