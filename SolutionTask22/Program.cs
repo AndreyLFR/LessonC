8f93c79ba87640dbb07d8a0cb40c2372b35a6db0
LFR <ooolfr@mail.ru>
2022-07-19 17:58:56 +0300

    Task22 квадраты числа от 1 до n

---
+++ SolutionTask22/Program.cs
@@ -0,0 +1,14 @@
+﻿void printSqr(int j)
+{
+    int sqrNumber = j * j;
+    Console.Write(sqrNumber);
+    Console.Write(", ");
+
+}
+
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < number + 1; i++)
+{
+    printSqr(i);
+}
