72722fa7fe8e83dcf0f78d8ae53e4c3fb055368b
LFR <ooolfr@mail.ru>
2022-07-19 23:14:33 +0300

    Сделал строго по заданию. Время поджимает, поэтому решил не пробовать со *

---
+++ HomeWorkTask23/Program.cs
@@ -0,0 +1,14 @@
+﻿void printCube(int j)
+{
+    int cubeNumber = j * j * j;
+    Console.Write(cubeNumber);
+    Console.Write(", ");
+
+}
+
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < number + 1; i++)
+{
+    printCube(i);
+}
