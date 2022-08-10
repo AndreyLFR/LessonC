02410c1aa726fb1b4e86b94249a3b22acf699b77
LFR <ooolfr@mail.ru>
2022-08-10 19:45:49 +0300

    Task28 factorial

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int inputNumber=int.Parse(Console.ReadLine()??"");
+
+int Factorial()
+{
+    int proiz=1;
+    int i=1;
+    while(i<inputNumber+1)
+    {
+        proiz=proiz*i;
+        i++;
+    }
+    return proiz;
+}
+
+Console.WriteLine(Factorial());
