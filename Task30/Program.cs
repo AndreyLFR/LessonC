c7dd5ef7fa3f0f9be44290358a3c82cf0250109d
LFR <ooolfr@mail.ru>
2022-08-10 20:31:26 +0300

    Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Random numberSintezator=new Random();
+
+void VariantNaive()
+{
+    int i=0;
+    Console.Write("[");
+    while(i<8-1)
+    {
+        Console.Write(numberSintezator.Next(0,2));
+        Console.Write(",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,2));
+    Console.Write("]");
+}
+
+VariantNaive();
