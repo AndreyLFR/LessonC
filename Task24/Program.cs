9606a7bbb03f316a7c90a5699ec058be67b8a374
LFR <ooolfr@mail.ru>
2022-08-10 18:35:50 +0300

    Gauss

---
+++ Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+
+int inputNumber=int.Parse(Console.ReadLine());
+int t;
+
+void VariantSimple()
+{
+    int sumOfNumber=0;
+    for(int i=1;i<=inputNumber;i++)
+    {
+        sumOfNumber+=i;
+    }
+Console.WriteLine("Сумма чисел от 1 до "+inputNumber+" :"+sumOfNumber);
+} 
+
+void VariantGauss()
+{
+    int sumOfNumber=0;
+    sumOfNumber=(1+inputNumber)*inputNumber/2;
+    Console.WriteLine("Сумма чисел от 1 до "+inputNumber+" :"+sumOfNumber);
+}
+
+t=Environment.TickCount;
+VariantSimple();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount-t);
+t=Environment.TickCount;
+VariantGauss();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount-t);
