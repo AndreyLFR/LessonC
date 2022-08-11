25e42b5b99471b2fa0530c12ddf6d00e3fa5a57e
LFR <ooolfr@mail.ru>
2022-08-11 18:08:42 +0300

    Сумма пол и отр чисел массива

---
+++ Task31/Program.cs
@@ -0,0 +1,35 @@
+﻿System.Random numberSintezator=new Random();
+
+int arrLength=12;
+int downNumber=-9;
+int upNumber=9;
+int sumPoz=0;
+int sumNeg=0;
+
+void GenArray()
+{
+    int i=0;
+    Console.Write("[");
+    while(i<arrLength-1)
+    {
+        int numb=numberSintezator.Next(downNumber,upNumber+1);
+        if(numb>0)
+        {sumPoz=sumPoz+numb;}
+        else{sumNeg=sumNeg+numb;}
+
+        Console.Write(numb);
+        Console.Write(",");
+        i++;
+    }
+    
+    int numberFinish=numberSintezator.Next(downNumber,upNumber+1);
+    if(numberFinish>0)
+        {sumPoz=sumPoz+numberFinish;}
+        else{sumNeg=sumNeg+numberFinish;}
+    Console.Write(numberFinish);
+    Console.Write("]");
+    Console.Write("Сумма пол значений "+sumPoz+". Сумма отр значений "+sumNeg);
+}
+
+
+GenArray();
