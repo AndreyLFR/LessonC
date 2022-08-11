d9829e7144ee1fac791af8337744d4a4278443a6
LFR <ooolfr@mail.ru>
2022-08-11 14:35:12 +0300

    сделал только решение со *

---
+++ HWTask29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+//*ввести с клавиатуры длину массива и диапазон элементов
+//решение только задачи со *
+
+System.Random numberSintezator=new Random();
+Console.Write("Введите длину массива: ");
+int arrLength=int.Parse(Console.ReadLine()??"");
+Console.Write("Введите нижнюю границу диапазона элементов: ");
+int downNumber=int.Parse(Console.ReadLine()??"");
+Console.Write("Введите верхнюю границу диапазона элементов: ");
+int upNumber=int.Parse(Console.ReadLine()??"");
+
+void GenArray()
+{
+    int i=0;
+    Console.Write("[");
+    while(i<arrLength-1)
+    {
+        Console.Write(numberSintezator.Next(downNumber,upNumber));
+        Console.Write(",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(downNumber,upNumber));
+    Console.Write("]");
+}
+
+GenArray();
