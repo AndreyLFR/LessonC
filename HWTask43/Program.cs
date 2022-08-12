a00b16b23d2bee548e4c4e9f20bea81971a4e346
LFR <ooolfr@mail.ru>
2022-08-13 00:23:13 +0300

    Задача решена без использования методов, так как в них нет острой необходимости. Силы кончились, задачку со * в следующий раз

---
+++ HWTask43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями
+//y=k1*x+b1
+//y=k2*x+b2
+//значения b1 b2 k1 k2 задает пользователь
+
+Console.Write("Введите значение b1: ");
+double b1=double.Parse(Console.ReadLine()??"");
+Console.Write("Введите значение b2: ");
+double b2=double.Parse(Console.ReadLine()??"");
+Console.Write("Введите значение k1: ");
+double k1=double.Parse(Console.ReadLine()??"");
+Console.Write("Введите значение k2: ");
+double k2=double.Parse(Console.ReadLine()??"");
+
+double x=(b2-b1)/(k1-k2);
+double y=y=k1*x+b1;
+Console.WriteLine("x="+x+", y="+y);
