6074c42d93169fe4d5069a3f875bd1caec96d648
LFR <ooolfr@mail.ru>
2022-08-12 20:20:28 +0300

    щиоусе

---
+++ Task45/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] CopyArrayStandartTool(int[] inputArray)
+{
+    int[] buferArray=new int[inputArray.Length];
+    inputArray.CopyTo(buferArray,0);
+    return buferArray;
+}
+
+object[] CopyArrayStandartToolObj(params object[] inputArray)
+{
+    object[] buferArray=new object[inputArray.Length];
+    inputArray.CopyTo(buferArray,0);
+    return buferArray;
+}
