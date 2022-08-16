fad0e483084161b65e0d7ffe097c1a32cd52bb94
LFR <ooolfr@mail.ru>
2022-08-16 16:59:19 +0300

    Проход диагонали 2 массива

---
+++ Task51/Program.cs
@@ -0,0 +1,61 @@
+﻿//метод возвращает двумерный массив, заполненный случайными цфрами
+int[,] FillingTwoDimArray(int countRow, int countColumn)
+{
+    int[,] outArray=new int[countRow, countColumn];
+    int i=0; int j=0;
+    System.Random numberSintezator=new System.Random();
+    while(i<countRow)
+    {
+        j=0;
+        while(j<countColumn)
+            {outArray[i,j]=numberSintezator.Next(0, 101);
+            j++;
+            }
+        i++;
+    }
+    return outArray;
+}
+
+//Сумма элементов по диагонали
+int CalculateDiagonale(int[,] inputArray)
+{
+    int i=0;
+    int sum=0;
+    while(i<inputArray.GetLength(0))
+    {
+        sum=sum+inputArray[i,i];
+        i++;            
+    }
+    return sum;
+}
+
+//массив цветов
+ConsoleColor[] col=new ConsoleColor[]{ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan,
+                                    ConsoleColor.DarkBlue, ConsoleColor.DarkCyan, ConsoleColor.DarkGray,
+                                    ConsoleColor.Green, ConsoleColor.DarkMagenta, ConsoleColor.DarkRed,
+                                    ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.DarkGreen,
+                                    ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.Red, ConsoleColor.Yellow};
+
+//Метод печатает цветной массив
+void PrintColorTwoDimArray(int[,] inputArray)
+{
+    int i=0; int j=0;
+    while(i<inputArray.GetLength(0))
+    {
+        j=0;
+        while(j<inputArray.GetLength(1))
+        {
+            Console.ForegroundColor=col[new System.Random().Next(0,16)];
+            Console.Write(inputArray[i,j]+" ");
+            Console.ResetColor();
+            j++;            
+        }
+        i++;
+        Console.Write("\n");
+    }
+}
+
+int[,] twoDimArray=FillingTwoDimArray(5, 5);
+PrintColorTwoDimArray(twoDimArray);
+int sumDionaleDimArray=CalculateDiagonale(twoDimArray);
+Console.Write(sumDionaleDimArray);
