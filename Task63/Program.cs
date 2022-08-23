e5d59359e8ebfcf174cd71bbcf585ad9df6ec18d
LFR <ooolfr@mail.ru>
2022-08-23 17:48:03 +0300

    Рекурсия

---
+++ Task63/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadData()
+{
+    Console.WriteLine("Введите число N : ");
+    return int.Parse(Console.ReadLine()??"");
+}
+
+int NumberPrint(int num)
+{
+    if(num==2) return 1;
+    else
+    {
+        num--;
+        Console.Write(NumberPrint(num)+" ");
+    }
+    return num;
+}
+
+int inputNumber=ReadData();
+NumberPrint(inputNumber);
