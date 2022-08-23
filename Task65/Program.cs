3fd62bd5e0e659455e84dbc79528e47e775a5b9a
LFR <ooolfr@mail.ru>
2022-08-23 18:02:44 +0300

    Рекурсия. Заполнение от m до  n

---
+++ Task65/Program.cs
@@ -0,0 +1,21 @@
+﻿// все натуральные числа от m до n
+
+int ReadData(string namePeremenay)
+{
+    Console.WriteLine("Введите число "+namePeremenay+": ");
+    return int.Parse(Console.ReadLine()??"");
+}
+
+void NaturNumberPrint(int m, int n)
+{
+    if(m==n) return;
+    else
+    {
+        Console.Write(m+" ");
+        NaturNumberPrint(m+1, n);        
+    }
+}
+
+int m=ReadData("M");
+int n=ReadData("N");
+NaturNumberPrint(m, n);
