c6f04cbc78f7df68e6e180397947baa72859cdb0
LFR <ooolfr@mail.ru>
2022-08-24 11:19:42 +0300

    сумма цифр в числе - рекурсия

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// сумма цифр введенного числа
+int ReadData(string namePeremenay)
+{
+    Console.WriteLine("Введите число "+namePeremenay+": ");
+    return int.Parse(Console.ReadLine()??"");
+}
+
+int Sum(int num)
+{
+    if(num>0) return num%10+Sum(num/10);
+    else return 0;
+}
+
+int n=ReadData("N");
+Console.WriteLine(Sum(n));
