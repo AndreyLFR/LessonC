f09a052007352ae468ec6d0586c49913a0277533
LFR <ooolfr@mail.ru>
2022-08-24 13:17:55 +0300

    Task64

---
+++ HWTask64/Program.cs
@@ -0,0 +1,34 @@
+﻿//64// Задайте значения M и N.Напишите программу, которая выведет все натуральные числа
+// в промежутке от M и N
+
+int ReadLine(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()??"");
+}
+
+void NumberPrintUp(int m, int n)
+{
+    if(m-1==n) return;
+    else
+    { 
+        Console.WriteLine(m+" ");
+        NumberPrintUp(m+1, n);
+    }
+}
+
+void NumberPrintDown(int m, int n)
+{
+    if(n==m-1) return;
+    else
+    { 
+        NumberPrintDown(m+1, n);
+        Console.WriteLine(m+" ");
+    }
+}
+
+int m=ReadLine("Введите число m: ");
+int n=ReadLine("Введите число n: ");
+NumberPrintUp(m, n);
+Console.Write("\n");
+NumberPrintDown(m, n);
